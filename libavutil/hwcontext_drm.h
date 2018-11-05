513922ae934cb4a7d399e03a9420ecb211ca45aa
Hertz Wang <wangh@rock-chips.com>
2018-11-05 20:56:00 +0800

    hwcontext_drm: internal frame allocation
    Change-Id: Iba12ba90f5395fb72cf39e96e266bb63daaec231
    Signed-off-by: Hertz Wang <wangh@rock-chips.com>

--- libavutil/hwcontext_drm.h
+++ libavutil/hwcontext_drm.h
@@ -25,11 +25,6 @@
 /**
  * @file
  * API-specific header for AV_HWDEVICE_TYPE_DRM.
- *
- * Internal frame allocation is not currently supported - all frames
- * must be allocated by the user.  Thus AVHWFramesContext is always
- * NULL, though this may change if support for frame allocation is
- * added in future.
  */
 
 enum {
@@ -53,6 +48,8 @@ typedef struct AVDRMObjectDescriptor {
 
     /**
      * DRM PRIME mapped virtual ptr for above fd.
+     *
+     * The content of this buffer must be readonly when acting decoder's out buffer.
      */
     void *ptr;
 
@@ -147,10 +144,13 @@ typedef struct AVDRMFrameDescriptor {
     AVDRMObjectDescriptor objects[AV_DRM_MAX_PLANES];
     /**
      * Number of layers in the frame.
+     *
+     * Set by users if need more than 1.
      */
     int nb_layers;
     /**
      * Array of layers in the frame.
+     * NOTE: total planes of layers must not be more than AV_NUM_DATA_POINTERS.
      */
     AVDRMLayerDescriptor layers[AV_DRM_MAX_PLANES];
 } AVDRMFrameDescriptor;
